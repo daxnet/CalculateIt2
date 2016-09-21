19e290b935257d0e06a8c8bd7723b0360392a8a2
daxnet <daxnet@outlook.com>
2016-09-21 12:47:02 +0800

    Added the unit test project.

---
+++ tests/CalculateIt2.Tests/EquationFormationTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CalculateIt2.Tests
+{
+    public class EquationFormationTests
+    {
+        [Fact]
+        public void TestTest()
+        {
+            Assert.True(1==1);
+        }
+    }
+}
